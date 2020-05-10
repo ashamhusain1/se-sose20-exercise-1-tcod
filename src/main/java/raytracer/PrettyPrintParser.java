1d6b3eeff36c2682c407eba4a13a9a518b0f82e4
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-11 01:34:13 +0530

    Update PrettyPrintParser.java. Added the depth of indentation.
    Added the indentation depth. Has mistakes in indentation which are caused by standalone tags.

--- src/main/java/raytracer/PrettyPrintParser.java
+++ src/main/java/raytracer/PrettyPrintParser.java
@@ -4,6 +4,8 @@ import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 
 public class PrettyPrintParser extends XMLParser{
+	
+	private int depth = 0;
 
 	public PrettyPrintParser(String fileName) {
 		super(fileName);
@@ -11,13 +13,21 @@ public class PrettyPrintParser extends XMLParser{
 	
 	@Override
 	public void enter(StartElement startElement) {
+		for (int i = 0; i < this.depth; i++) {
+			System.out.print("    ");
+		}
 		System.out.print("→");
 		System.out.println(startElement.getName());
+		this.depth++;
 	}
 	
 	@Override
 	public void exit(EndElement endElement) {
+		for (int i = 0; i < this.depth; i++) {
+			System.out.print("    ");
+		}
 		System.out.print("←");
 		System.out.println(endElement.getName());
+		this.depth--;
 	}
 }
