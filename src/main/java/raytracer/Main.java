ebb487fe979cbb3ffb18f86ee14baef9e9f29c0f
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-11 02:31:50 +0530

    Update Main.java
    Added comments

--- src/main/java/raytracer/Main.java
+++ src/main/java/raytracer/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
 	
-	static final String PATH = "./resources/";
+	static final String PATH = "./resources/"; // Common prefix for the files.
 	
 	public static void main(String [] args) throws Exception {
 		
@@ -15,11 +15,13 @@ public class Main {
 		files.add("test2.x3d");
 		files.add("test3.x3d");
 		for (int i = 0; i < files.size(); i++) {
-			String file = PATH + files.get(i); // path to file
-
+			String file = PATH + files.get(i); // Path to file.
+			
+			// Object of XMLParser class which can call the parse method.
 			XMLParser docXML = new XMLParser(file);
 			docXML.parse();
-
+			
+			// Object of PrettyPrintParser class which can call the parse method.
 			PrettyPrintParser docPPP = new PrettyPrintParser(file);
 			docPPP.parse();
 		}
