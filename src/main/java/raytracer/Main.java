0b55299518de33015ea3040c3cc5d980039300a5
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-11 02:10:11 +0530

    Improved tester codes.
    Improved the way the files are tested. Used an ArrayList to store the path to each file and looped over it.

--- src/main/java/raytracer/Main.java
+++ src/main/java/raytracer/Main.java
@@ -1,45 +1,27 @@
 package raytracer;
 
+import java.util.ArrayList;
+
 public class Main {
+	
+	static final String PATH = "./resources/";
+	
 	public static void main(String [] args) throws Exception {
-		String file1 = "./resources/foo.xml"; // path to foo.xml
-
-		XMLParser docXML1 = new XMLParser(file1);
-		docXML1.parse();
-
-		PrettyPrintParser docPPP1 = new PrettyPrintParser(file1);
-		docPPP1.parse();
-
-		String file2 = "./resources/scene.x3d"; // path to scene.x3d
-
-		XMLParser docXML2 = new XMLParser(file2);
-		docXML2.parse();
-
-		PrettyPrintParser docPPP2 = new PrettyPrintParser(file2);
-		docPPP2.parse();
-
-		String file3 = "./resources/test1.x3d"; // path to test1.x3d
-
-		XMLParser docXML3 = new XMLParser(file3);
-		docXML3.parse();
-
-		PrettyPrintParser docPPP3 = new PrettyPrintParser(file3);
-		docPPP3.parse();
-
-		String file4 = "./resources/test2.x3d"; // path to test2.x3d
-
-		XMLParser docXML4 = new XMLParser(file4);
-		docXML4.parse();
-
-		PrettyPrintParser docPPP4 = new PrettyPrintParser(file4);
-		docPPP4.parse();
 		
-		String file5 = "./resources/test3.x3d"; // path to test3.x3d
+		ArrayList<String> files = new ArrayList<String>();
+		files.add("foo.xml");
+		files.add("scene.x3d");
+		files.add("test1.x3d");
+		files.add("test2.x3d");
+		files.add("test3.x3d");
+		for (int i = 0; i < files.size(); i++) {
+			String file = PATH + files.get(i); // path to file
 
-		XMLParser docXML5 = new XMLParser(file5);
-		docXML5.parse();
+			XMLParser docXML = new XMLParser(file);
+			docXML.parse();
 
-		PrettyPrintParser docPPP5 = new PrettyPrintParser(file5);
-		docPPP5.parse();
+			PrettyPrintParser docPPP = new PrettyPrintParser(file);
+			docPPP.parse();
+		}
 	}
 }
