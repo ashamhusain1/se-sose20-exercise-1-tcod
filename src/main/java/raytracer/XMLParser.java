7d0e07b844538ed8fb41a1c39182d720357c97d7
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-10 05:40:17 +0530

    Create XMLParser.java
    Created the "XMLParser" class with the required methods which are not yet implemented.

---
+++ src/main/java/raytracer/XMLParser.java
@@ -0,0 +1,22 @@
+package raytracer;
+
+public class XMLParser {
+	
+	String fileName;
+	
+	public XMLParser(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public void enter(String tagName) {
+		//TODO Implementation
+	}
+	
+	public void exit(String tagName) {
+		//TODO Implementation
+	}
+	
+	public void parse() {
+		//TODO Implementation
+	}
+}
