209fc3cf20b7a4d805561cecedd1f5e0e1e9665f
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-11 01:20:31 +0530

    Update Main.java. Added method calls.
    Added the method calls to the parse method from XMLParser and PrettyPrintParser to test the funtionality.

--- src/main/java/raytracer/Main.java
+++ src/main/java/raytracer/Main.java
@@ -3,5 +3,11 @@ package raytracer;
 public class Main {
     public static void main(String [] args) throws Exception {
         String file = "./resources/foo.xml"; // path to foo.xml
+        
+        XMLParser docXML = new XMLParser(file);
+        docXML.parse();
+        
+        PrettyPrintParser docPPP = new PrettyPrintParser(file);
+        docPPP.parse();
     }
 }
