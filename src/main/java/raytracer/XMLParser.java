bf5cb9b708b05d432abc6b96c2bb6f191397725c
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-11 02:37:39 +0530

    Update XMLParser.java
    Added comments.

--- src/main/java/raytracer/XMLParser.java
+++ src/main/java/raytracer/XMLParser.java
@@ -12,23 +12,40 @@ import javax.xml.stream.XMLStreamException;
 
 public class XMLParser {
 	
-	String fileName;
+	String fileName; // Name of the file.
 	
+	/**
+	 * Constructor.
+	 * @param fileName
+	 */
 	public XMLParser(String fileName) {
 		this.fileName = fileName;
 	}
 
+	/**
+	 * Method for printing the encountered opening tag.
+	 * @param startElement
+	 */
 	public void enter(StartElement startElement) {
 		System.out.println(startElement.getName());
 	}
 
+	/**
+	 * Method for printing the encountered closing tag.
+	 * @param endElement
+	 */
 	public void exit(EndElement endElement) {
 		System.out.println(endElement.getName());
 	}
 
+	/**
+	 * Method to parse through the file and search opening and closing tags.
+	 * @throws FileNotFoundException
+	 * @throws XMLStreamException
+	 */
 	public void parse() throws FileNotFoundException, XMLStreamException {
 		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-		XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(this.fileName));
+		XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(this.fileName)); // Reads and handles the events.
 		while (xmlEventReader.hasNext()) {
 		    XMLEvent nextEvent = xmlEventReader.nextEvent();
 		    if (nextEvent.isStartElement()) {
