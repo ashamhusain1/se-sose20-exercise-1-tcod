b1051a9b84177fad87b3bf9e9b892a63b0cb84eb
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-11 01:01:46 +0530

    Update XMLParser.java
    Implementation of parse method added. This uses XMLEventReader class to read the input file and detect the tags in it. The tags are tested for being a start tag or an end tag.

--- src/main/java/raytracer/XMLParser.java
+++ src/main/java/raytracer/XMLParser.java
@@ -1,7 +1,14 @@
 package raytracer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
 
 public class XMLParser {
 	
@@ -14,12 +21,25 @@ public class XMLParser {
 	public void enter(StartElement startElement) {
 		//TODO Implementation
 	}
-	
+
 	public void exit(EndElement endElement) {
 		//TODO Implementation
+		
 	}
-	
-	public void parse() {
-		//TODO Implementation
+
+	public void parse() throws FileNotFoundException, XMLStreamException {
+		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(new FileInputStream(this.fileName));
+		while (xmlEventReader.hasNext()) {
+		    XMLEvent nextEvent = xmlEventReader.nextEvent();
+		    if (nextEvent.isStartElement()) {
+			    StartElement startElement = nextEvent.asStartElement();
+			    this.enter(startElement);
+			}
+		    if (nextEvent.isEndElement()) {
+		        EndElement endElement = nextEvent.asEndElement();
+		        this.exit(endElement);
+		    }
+		}
 	}
 }
