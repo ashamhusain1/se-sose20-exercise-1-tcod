49b18c7eb45302d39eeb1464fdcd681e48c2b812
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-11 02:42:36 +0530

    Update PrettyPrintParser.java
    Added comments.

--- src/main/java/raytracer/PrettyPrintParser.java
+++ src/main/java/raytracer/PrettyPrintParser.java
@@ -5,8 +5,12 @@ import javax.xml.stream.events.StartElement;
 
 public class PrettyPrintParser extends XMLParser{
 	
-	private int depth = 0;
+	private int depth = 0; // Depth of indentation
 
+	/**
+	 * Constructor.
+	 * @param fileName
+	 */
 	public PrettyPrintParser(String fileName) {
 		super(fileName);
 	}
@@ -14,18 +18,18 @@ public class PrettyPrintParser extends XMLParser{
 	@Override
 	public void enter(StartElement startElement) {
 		for (int i = 0; i < this.depth; i++) {
-			System.out.print("    ");
+			System.out.print("    "); // Printing a tab space for indentation.
 		}
 		System.out.print("→");
 		System.out.println(startElement.getName());
-		this.depth++;
+		this.depth++; // Incrementing the depth.
 	}
 	
 	@Override
 	public void exit(EndElement endElement) {
-		this.depth--;
+		this.depth--; // Decrementing the depth.
 		for (int i = 0; i < this.depth; i++) {
-			System.out.print("    ");
+			System.out.print("    "); // Printing a tab space for indentation.
 		}
 		System.out.print("←");
 		System.out.println(endElement.getName());
