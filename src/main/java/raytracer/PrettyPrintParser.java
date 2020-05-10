b2fb5fd7412db4da4d47d2b7c14ce239fda8871f
ashamhusain <56700095+ashamhusain@users.noreply.github.com>
2020-05-10 05:41:18 +0530

    Update PrettyPrintParser.java
    Added the basic structure of the code.

--- src/main/java/raytracer/PrettyPrintParser.java
+++ src/main/java/raytracer/PrettyPrintParser.java
@@ -1,1 +1,20 @@
+package raytracer;
 
+public class PrettyPrintParser extends XMLParser{
+
+	public PrettyPrintParser(String fileName) {
+		super(fileName);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void enter(String tagName) {
+		//TODO Implementation
+	}
+	
+	@Override
+	public void exit(String tagName) {
+		//TODO implementation
+	}
+
+}
